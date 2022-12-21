f8cb0518c78b3e2b59e44a8dcd85f9054f7e52f6
kkulek <106318909+kkulek@users.noreply.github.com>
2022-12-21 13:53:37 +0100

    added basic colors, lineheight for Hero, fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				dark: "rgb(52, 52, 52)",
+				accent: "rgb(3, 166, 144)",
+			},
+			lineHeight: {
+				'hero-headline': '60px',
+				'body-regular': '34px'
+			}
+		},
+		fontFamily: {
+			montserrat: ["Montserrat", "sans-serif"],
+			corbel: ["Corbel", "sans-serif"],
+		},
 	},
 	plugins: [],
-}
+};
